62970aaa96bb478b1e7a6dd8d70cabd00c96c77d
Glen <0xJozeth@gmail.com>
2022-11-12 21:20:26 -0500

    homepage changes

--- components/HomepageComponents/HeroComponent/Hero-tw.jsx
+++ components/HomepageComponents/HeroComponent/Hero-tw.jsx
@@ -3,16 +3,19 @@ import Banner from '/components/Banner/Banner.js';
 import Link from 'next/link';
 import Image from 'next/image';
 import arrow from '/public/arrow@1x.png';
+import group from '/public/microsoftteams-image@1x.png';
 
 import { handleToggle, handleSlide } from '/components/Header/Navbar.jsx';
 
 // IMPORT ITERATIVE MAPPING DATA
 import { linkText, vimeoHeroVideo } from './heroData';
 
+import WeHaveAGrace from '../SectionWeHaveAGrace/WeHaveAGrace';
+
 function HeroTW() {
 	return (
 		<>
-			<div className='flex flex-col max-gitw-[355px] mx-auto overflow-hidden'>
+			<div className='flex flex-col max-w-[355px] mx-auto overflow-hidden '>
 				<div className='flex relative justify-center items-center'>
 					<div className='absolute top-0 left-0 right-0 z-30 mix-blend-overlay text-[#f8f8f8] max-w-min mx-auto'>
 						<h1 className='font-display font-bold text-[44px]'>
@@ -30,7 +33,7 @@ function HeroTW() {
 							src='https://player.vimeo.com/video/744290982?h=26579d8c5a&controls=0&badge=0&autoplay=1&muted=1&loop=1&autopause=0&player_id=0&app_id=58479'
 							frameBorder='0'
 							width={305}
-							height={667}
+							height={567}
 							placeholder={'blur'}
 						></iframe>
 					</div>
@@ -46,16 +49,77 @@ function HeroTW() {
 						</h1>
 					</div>
 				</div>
-				<div className='flex my-12 lg:hidden place-self-center'>
-					<p className=' font-display text-xl lg:text-4xl text-[#7C7C7C] px-4 max-w-[355px] lg:max-w-2xl z-10'>
-						We’re so glad you’re here! At <strong>Celebration Church</strong>,
-						we prioritize Jesus’ mission to spread the gospel. We are a global
-						house comprised of many rooms around the world, with headquarters
-						residing in Jacksonville, Florida. We’d love to connect with you
-						online or at a location near you!
-					</p>
+				<div className='flex flex-col py-4'>
+					<div className='flex lg:hidden place-self-center'>
+						<p className=' font-display text-[18px] lg:text-4xl text-[#7C7C7C] px-4 max-w-[355px] lg:max-w-2xl z-10'>
+							We’re so glad you’re here! At <strong>Celebration Church</strong>,
+							we prioritize Jesus’ mission to spread the gospel. We are a global
+							house comprised of many rooms around the world, with headquarters
+							residing in Jacksonville, Florida. We’d love to connect with you
+							online or at a location near you!
+						</p>
+					</div>
+					<div className='block my-4 lg:hidden'>
+						<button className='flex mx-auto items-center font-display text-[14px] font-semibold cursor-pointer z-10 lg:text-[1.5vw] col-span-2 place-self-center'>
+							WATCH THE LATEST MESSAGE
+							<div className='flex mx-4 h-6 w-14'>
+								<Image
+									src={arrow}
+									// height={22}
+									// width={50}
+									alt='watch-the-latest-message'
+								/>
+							</div>
+						</button>
+					</div>
+				</div>
+				<div className='flex flex-col max-w-[355px] my-8'>
+					<div className='relative flex flex-col '>
+						<div className='absolute top-0 left-0 right-0 flex font-bold font-display text-[44px] justify-end text-[#f8f8f8] mix-blend-overlay z-30'>
+							<h2 className=''>
+								FIND A&nbsp;
+								<br />
+								<span className='font-kepler italic font-[38px]'>LOCATION</span>
+								&nbsp;
+								<br />
+								NEAR YOU
+							</h2>
+						</div>
+						<div className='absolute top-0 left-0 right-0 flex font-bold font-display text-[44px] justify-end z-20'>
+							<h2 className=''>
+								FIND A&nbsp;
+								<br />
+								<span className='font-kepler italic font-[38px]'>LOCATION</span>
+								&nbsp;
+								<br />
+								NEAR YOU
+							</h2>
+						</div>
+						<div className='flex justify-end w-[268px] h-[268px] mt-16 z-20'>
+							<Image src={group} alt='' />
+						</div>
+					</div>
+					<div className='flex my-4 justify-end'>
+						<button className='flex items-center cursor-pointer z-10'>
+							<p className='font-display text-[14px] font-semibold'>
+								COME VISIT US
+							</p>
+							<div className='flex mx-4 h-6 w-14'>
+								<Image
+									src={arrow}
+									// height={22}
+									// width={50}
+									alt='watch-the-latest-message'
+								/>
+							</div>
+						</button>
+					</div>
+				</div>
+				<div className='max-w-[355px]'>
+					<WeHaveAGrace />
 				</div>
 			</div>
+
 			<div className='lg:grid grid-cols-2 grid-row lg:grid-rows-2 '>
 				<div className='flex flex-col items-center lg:justify-evenly lg:items-start lg:col-span-2 lg:col-start-1 lg:row-start-1 max-w-[1536px]'>
 					<div className='flex flex-col mx-4 text-[3.15rem] font-display font-black'>
