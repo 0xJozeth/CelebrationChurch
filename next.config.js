af3d5f1acc0d14c9e5e44b94c45f228beaf487f4
Glen <0xJozeth@gmail.com>
2022-11-23 15:15:29 -0500

    updating Give mobile

--- next.config.js
+++ next.config.js
@@ -6,10 +6,23 @@ const nextConfig = {
 	swcMinify: true,
 };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
 
 // module.exports = {
 // 	images: {
 // 		domains: ['https://mdbootstrap.com'],
 // 	},
 // };
+
+module.exports = {
+	webpack: (config, { isServer }) => {
+		if (!isServer) {
+			// don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+			config.resolve.fallback = {
+				fs: false,
+			};
+		}
+
+		return config;
+	},
+};
